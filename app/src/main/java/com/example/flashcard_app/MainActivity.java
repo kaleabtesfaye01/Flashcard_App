d5cbc9534bb5c2ca14c6b7394865026d7964f0a6
kaleabtesfaye01 <kaleabtesfaye01@gmail.com>
2022-03-12 13:30:21 -0500

    Update for Optional Tasks

--- app/src/main/java/com/example/flashcard_app/MainActivity.java
+++ app/src/main/java/com/example/flashcard_app/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.flashcard_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,63 +21,43 @@ public class MainActivity extends AppCompatActivity {
         TextView choice3 = findViewById(R.id.choice3);
         ImageView hideChoice = findViewById(R.id.toggle_choices_visibility);
 
-        question.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                question.setVisibility(View.INVISIBLE);
-                answer.setVisibility(View.VISIBLE);
-            }
+        question.setOnClickListener(v -> {
+            question.setVisibility(View.INVISIBLE);
+            answer.setVisibility(View.VISIBLE);
         });
 
-        answer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                answer.setVisibility(View.INVISIBLE);
-                question.setVisibility(View.VISIBLE);
-            }
+        answer.setOnClickListener(v -> {
+            answer.setVisibility(View.INVISIBLE);
+            question.setVisibility(View.VISIBLE);
         });
 
-        choice1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                choice1.setBackgroundColor(getResources().getColor(R.color.my_red_color,null));
-                choice3.setBackgroundColor(getResources().getColor(R.color.my_green_color,null));
-            }
+        choice1.setOnClickListener(v -> {
+            choice1.setBackgroundColor(getResources().getColor(R.color.my_red_color,null));
+            choice3.setBackgroundColor(getResources().getColor(R.color.my_green_color,null));
         });
 
-        choice2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                choice2.setBackgroundColor(getResources().getColor(R.color.my_red_color,null));
-                choice3.setBackgroundColor(getResources().getColor(R.color.my_green_color,null));
-            }
+        choice2.setOnClickListener(v -> {
+            choice2.setBackgroundColor(getResources().getColor(R.color.my_red_color,null));
+            choice3.setBackgroundColor(getResources().getColor(R.color.my_green_color,null));
         });
 
-        choice3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                choice3.setBackgroundColor(getResources().getColor(R.color.my_green_color,null));
-            }
-        });
+        choice3.setOnClickListener(v -> choice3.setBackgroundColor(getResources().getColor(R.color.my_green_color,null)));
 
         final boolean[] isShowingAnswers = new boolean[1];
         isShowingAnswers[0] = true;
-        hideChoice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isShowingAnswers[0]) {
-                    hideChoice.setImageResource(R.drawable.eye1);
-                    choice1.setVisibility(View.INVISIBLE);
-                    choice2.setVisibility(View.INVISIBLE);
-                    choice3.setVisibility(View.INVISIBLE);
-                    isShowingAnswers[0] = false;
-                }else if (!isShowingAnswers[0]){
-                    hideChoice.setImageResource(R.drawable.eye2);
-                    choice1.setVisibility(View.VISIBLE);
-                    choice2.setVisibility(View.VISIBLE);
-                    choice3.setVisibility(View.VISIBLE);
-                    isShowingAnswers[0] = true;
-                }
+        hideChoice.setOnClickListener(v -> {
+            if (isShowingAnswers[0]) {
+                hideChoice.setImageResource(R.drawable.eye1);
+                choice1.setVisibility(View.INVISIBLE);
+                choice2.setVisibility(View.INVISIBLE);
+                choice3.setVisibility(View.INVISIBLE);
+                isShowingAnswers[0] = false;
+            }else{
+                hideChoice.setImageResource(R.drawable.eye2);
+                choice1.setVisibility(View.VISIBLE);
+                choice2.setVisibility(View.VISIBLE);
+                choice3.setVisibility(View.VISIBLE);
+                isShowingAnswers[0] = true;
             }
         });
 
